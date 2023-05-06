94591d2494521094c63aa4b3ddb50a6e2fa24ec2
Federico Cavallo <fcmde1995@gmail.com>
2023-05-06 16:35:13 -0300

    MR:PR fixes

--- src/resources/member.js
+++ src/resources/member.js
@@ -5,38 +5,56 @@ const express = require('express');
 const router = express.Router();
 const membersData = require('../data/member.json');
 
-router.put('/members/update/:id', (req, res) => {
+router.put('/update/:id?', (req, res) => {
   const memberId = parseInt(req.params.id, 10);
   const updatedMember = req.body;
-  const members = JSON.parse(fs.readFileSync('src/data/member.json', 'utf8'));
-  const memberIndex = members.findIndex((member) => member.id === memberId);
-  const memberemail = membersData.find((m) => m.email === updatedMember.email);
+  const memberIndex = membersData
+    .findIndex((member) => member.id === memberId);
+  const memberemail = membersData
+    .find((m) => m.email === updatedMember.email);
+  if (!memberId) {
+    return res.status(400).json({ message: 'ID is required' });
+  }
+  if (!req.body) {
+    return res.status(400).json({ message: 'Request body is required' });
+  }
   if (memberIndex >= 0) {
     if (!memberemail) {
-      members[memberIndex] = { ...members[memberIndex], ...updatedMember };
-      fs.writeFileSync('src/data/member.json', JSON.stringify(members));
-      res.json(members);
-    } else if (memberemail.id === memberId) {
-      members[memberIndex] = { ...members[memberIndex], ...updatedMember };
-      fs.writeFileSync('src/data/member.json', JSON.stringify(members));
-      res.json(members);
-    } else {
-      res.status(404).json({ message: 'Email already exist on the database' });
+      membersData[memberIndex] = {
+        ...membersData[memberIndex],
+        ...updatedMember,
+      };
+      fs.writeFileSync('src/data/member.json', JSON.stringify(membersData));
+      return res.json(membersData);
     }
-  } else {
-    res.status(404).json({ message: 'Member not found' });
+    if (memberemail.id === memberId) {
+      membersData[memberIndex] = {
+        ...membersData[memberIndex],
+        ...updatedMember,
+      };
+      fs.writeFileSync('src/data/member.json', JSON.stringify(membersData));
+      return res.json(membersData);
+    }
+    return res
+      .status(404)
+      .json({ message: 'Email already exist on the database' });
   }
+  return res.status(404).json({ message: 'Member not found' });
 });
 
-router.delete('/members/delete/:id', (req, res) => {
+router.delete('/delete/:id', (req, res) => {
   const memberId = parseInt(req.params.id, 10);
-  const members = JSON.parse(fs.readFileSync('src/data/member.json', 'utf8'));
-  const filteredMembers = members.filter((member) => member.id !== memberId);
-  if (filteredMembers.length < members.length) {
-    fs.writeFileSync('src/data/member.json', JSON.stringify(filteredMembers));
-    res.json(filteredMembers);
+  const filteredMembers = membersData
+    .filter((member) => member.id !== memberId);
+  if (req.params.id) {
+    if (filteredMembers.length < membersData.length) {
+      fs.writeFileSync('src/data/member.json', JSON.stringify(filteredMembers));
+      res.json(filteredMembers);
+    } else {
+      res.status(404).json({ message: 'Member not found' });
+    }
   } else {
-    res.status(404).json({ message: 'Member not found' });
+    res.json({ message: 'Id needed to delete a user' });
   }
 });
 
