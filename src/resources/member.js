c0217c61d3a932bf8eb42fd33290b8e8a67d0c90
Ignacio <igna.galceran@gmail.com>
2023-05-07 17:32:16 -0300

    MR: Fixing functions get getById and post

--- src/resources/member.js
+++ src/resources/member.js
@@ -6,41 +6,24 @@ const router = express.Router();
 const membersData = require('../data/member.json');
 
 router.get('/get', (req, res) => {
-  const { firstName, lastName, location } = req.query;
-  let filteredMembers = membersData;
-  if (firstName) {
-    filteredMembers = filteredMembers.filter(
-      (member) => member.first_name === firstName,
-    );
+  if (membersData.length === 0) {
+    return res.status(400).json({ message: 'Members data is empty' });
   }
-  if (lastName) {
-    filteredMembers = filteredMembers.filter(
-      (member) => member.last_name === lastName,
-    );
-  }
-  if (location) {
-    filteredMembers = filteredMembers.filter(
-      (member) => member.location === location,
-    );
-  }
-  if (filteredMembers.length === 0) {
-    return res.status(404).json({ message: 'Member not found' });
-  }
-  return res.json(filteredMembers);
+  return res.send(membersData);
 });
 
 router.get('/getById/:id?', (req, res) => {
   if (!req.params.id) {
-    return res.status(404).json({ message: 'Id is required' });
+    return res.status(400).json({ message: 'Id is required' });
   }
   if (Number.isNaN(Number(req.params.id))) {
-    return res.status(404).json({ message: 'Id must be a number' });
+    return res.status(400).json({ message: 'Id must be a number' });
   }
   const member = membersData.find((m) => m.id.toString() === req.params.id);
   if (!member) {
     return res.status(404).json({ message: 'Member not found' });
   }
-  return res.json(member);
+  return res.send(member);
 });
 
 router.post('/post', (req, res) => {
@@ -50,22 +33,16 @@ router.post('/post', (req, res) => {
     const memberEmail = membersData.find((m) => m.email === body.email);
     const memberDni = membersData.find((m) => m.dni === body.dni);
     if (memberEmail) {
-      return res
-        .status(404)
-        .json({ message: 'A member with that email already exists' });
+      return res.status(400).json({ message: 'A member with that email already exists' });
     }
     if (memberDni) {
-      return res
-        .status(404)
-        .json({ message: 'A member with that dni already exists' });
+      return res.status(400).json({ message: 'A member with that dni already exists' });
     }
     membersData.push(body);
-    fs.writeFileSync('src/data/member.json', JSON.stringify(membersData));
-    return res.json(membersData);
+    fs.writeFileSync('src/data/member.json', JSON.stringify(membersData, null, 2));
+    return res.status(200).json({ message: 'Member added successfully' });
   }
-  return res
-    .status(404)
-    .json({ message: 'Fields are required' });
+  return res.status(400).json({ message: 'Fields are required' });
 });
 
 module.exports = router;
