a8c36c4612a26ace39ad74ee2e4f51f054a82343
unknown <ivanjukonis@hotmail.com>
2023-05-07 23:38:25 -0300

    MR: Router activities delete method completed

--- src/resources/activity.js
+++ src/resources/activity.js
@@ -8,7 +8,6 @@ const router = express.Router();
 router.put('/update/:id', (req, res) => {
   const activityId = req.params.id;
   const { name, description } = req.body;
-
   // eslint-disable-next-line no-shadow
   const activity = activities.find((activity) => activity.id === Number(activityId));
   if (!activity) {
@@ -21,6 +20,22 @@ router.put('/update/:id', (req, res) => {
       return res.status(500).json({ success: false, msg: 'The activity could not be modified.' });
     }
     return res.status(200).json({ success: true, activity });
+  });
+});
+
+// eslint-disable-next-line consistent-return
+router.delete('/delete/:id', (req, res) => {
+  const activityId = req.params.id;
+  const index = activities.findIndex((activity) => activity.id === Number(activityId));
+  if (index === -1) {
+    return res.status(404).json({ success: false, msg: 'Activity not found.' });
+  }
+  const deletedActivity = activities.splice(index, 1);
+  fs.writeFile('src/data/activity.json', JSON.stringify(activities, null, 2), (err) => {
+    if (err) {
+      return res.status(500).json({ success: false, msg: 'The activity could not be deleted.' });
+    }
+    return res.status(200).json({ success: true, msg: `Activity ${activityId} deleted successfully`, data: deletedActivity[0] });
   });
 });
 
