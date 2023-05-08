8d704b34557dafb5ca52d3778e51d26c6dbb9bab
lupomartin <lupomartin2003@gmail.com>
2023-05-07 23:56:11 -0300

    MR: made the corrections

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@
 import express from 'express';
 import cors from 'cors';
 
-const admins2 = require('./resources/admins');
-
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+const admins = require('./resources/admins');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -17,15 +15,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
-});
-
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
-
-app.use(admins2);
+app.use(admins);
--- src/resources/admins.js
+++ src/resources/admins.js
@@ -3,30 +3,41 @@ const fs = require('fs');
 const admins = require('../data/admins.json');
 
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send(admins);
-});
-
-router.get('/:id', (req, res) => {
-  const adminsID = req.params.id;
-  const foundAdmin = admins.find((admin) => admin.id.toString() === adminsID);
-  if (foundAdmin) {
-    res.send(foundAdmin);
+router.get('/get', (req, res) => {
+  if (admins.length) {
+    res.send(admins);
   } else {
-    res.send('Admin not Found');
+    res.send('Admins not found');
   }
 });
 
-router.post('/', (req, res) => {
+router.get('/getById/:id', (req, res) => {
+  const adminsID = req.params.id;
+  if (!adminsID) {
+    res.send('The admin id is required');
+  } else {
+    const foundAdmin = admins.find((admin) => admin.id.toString() === adminsID);
+    if (foundAdmin) {
+      res.send(foundAdmin);
+    } else {
+      res.send('Admin not Found');
+    }
+  }
+});
+
+router.post('/addAdmin', (req, res) => {
   const newAdmin = req.body;
   admins.push(newAdmin);
-  fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 2), (err) => {
-    if (err) {
-      res.send('Error! User cannot not be created');
-    } else {
-      res.send('Admin created!');
-    }
-  });
+  if (Object.entries(newAdmin).length === 0) {
+    res.send('Admin is required');
+  } else {
+    fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 2), (err) => {
+      if (err) {
+        res.send('Error! User cannot not be created');
+      } else {
+        res.send('Admin created!');
+      }
+    });
+  }
 });
 module.exports = router;
