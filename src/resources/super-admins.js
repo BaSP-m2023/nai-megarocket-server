50f422d1502597bef36724d6753d42ac6be5d5d6
Ignacio <igna.galceran@gmail.com>
2023-05-08 19:33:25 -0300

    MR: superadmins get, getById and post functions fixed

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -6,44 +6,42 @@ const router = express.Router();
 const superAdmin = require('../data/super-admins.json');
 
 router.get('/get', (req, res) => {
-  const { email } = req.query;
-  let filteredSuperA = superAdmin;
-  if (email) {
-    filteredSuperA = filteredSuperA.filter((sa) => sa.email === email);
+  if (superAdmin.length === 0) {
+    return res.status(400).json({ message: 'Super admins data is empty' });
   }
-  if (filteredSuperA.length === 0) {
-    return res.status(404).json({ message: 'Super admin not found' });
-  }
-  return res.json(filteredSuperA);
+  return res.send(superAdmin);
 });
 
 router.get('/getById/:id?', (req, res) => {
   if (!req.params.id) {
-    return res.status(404).json({ message: 'Id is required' });
+    return res.status(400).json({ message: 'Id is required' });
   }
   if (Number.isNaN(Number(req.params.id))) {
-    return res.status(404).json({ message: 'Id must be a number' });
+    return res.status(400).json({ message: 'Id must be a number' });
   }
   const superA = superAdmin.find((m) => m.id.toString() === req.params.id);
   if (!superA) {
     return res.status(404).json({ message: 'Super admin not found' });
   }
-  return res.json(superA);
+  return res.send(superA);
 });
 
 router.post('/post', (req, res) => {
   const id = superAdmin.length + 1;
+  if (Object.entries(req.body).length === 0) {
+    return res.status(400).json({ message: 'you have to specify email and password' });
+  }
   const body = { id, ...req.body };
   if (Object.values(body).every((el) => el !== '')) {
     const superAEmail = superAdmin.find((s) => s.email === body.email);
     if (superAEmail) {
-      return res.status(404).json({ message: 'Super admin with that email already exists' });
+      return res.status(400).json({ message: 'Super admin with that email already exists' });
     }
     superAdmin.push(body);
-    fs.writeFileSync('src/data/super-admins.json', JSON.stringify(superAdmin));
-    return res.json(superAdmin);
+    fs.writeFileSync('src/data/super-admins.json', JSON.stringify(superAdmin, null, 2));
+    return res.send(superAdmin);
   }
-  return res.status(404).json({ message: 'Fields are required' });
+  return res.status(400).json({ message: 'Fields are required' });
 });
 
 module.exports = router;
