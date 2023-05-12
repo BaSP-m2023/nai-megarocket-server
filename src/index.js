7656ab45a2c1e9cf4ee4ad1c3afe17e8e5d91143
Micaela A. Rossi <micaelarrossi@gmail.com>
2023-05-11 21:11:16 -0300

    MR-1: Corrected for merge

--- src/index.js
+++ src/index.js
@@ -16,10 +16,6 @@ mongoose.connect(DB_URL)
   .catch((error) => console.log('Error: ', error));
 app.use('/api', router);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,20 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-const subs = require('../controllers/subscription');
-const admin = require('../controllers/admins');
-const member = require('../controllers/member');
-const superAdmin = require('../controllers/super-admins');
+const subscriptions = require('../controllers/subscription');
+const admins = require('../controllers/admins');
+const members = require('../controllers/member');
+const superAdmins = require('../controllers/super-admins');
 const classes = require('../controllers/class');
-const trainer = require('../controllers/trainer');
-const activity = require('../controllers/activity');
+const trainers = require('../controllers/trainer');
+const activities = require('../controllers/activity');
 
-router.use('/members', member);
-router.use('/admins', admin);
-router.use('/superAdmins', superAdmin);
-router.use('/class', classes);
-router.use('/trainer', trainer);
-router.use('/subscription', subs);
-router.use('/activities', activity);
+router.use('/members', members);
+router.use('/admins', admins);
+router.use('/super-admins', superAdmins);
+router.use('/classes', classes);
+router.use('/trainers', trainers);
+router.use('/subscriptions', subscriptions);
+router.use('/activities', activities);
 
 module.exports = router;
