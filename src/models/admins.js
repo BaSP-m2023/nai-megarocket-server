e3f53d1bf9f939b36b60f06b519f4f35ab2d3dc1
Juan.Manuel <juanmlantermo@gmail.com>
2023-05-15 18:49:28 -0300

    MR-08: admins routes update

--- src/models/admins.js
+++ src/models/admins.js
@@ -2,53 +2,52 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const adminSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 10
+const adminSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 10,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    dni: {
+      type: Number,
+      required: true,
+      min: 10000000,
+      max: 99999999,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      min: 10,
+      max: 10,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowecase: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 25,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 4,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    minLength:3,
-    maxLength: 20,
-  },
-  dni: {
-    type: Number,
-    required: true,
-    min: 10000000,
-    max: 99999999,
-  },
-  phone: {
-    type: Number,
-    required : true,
-    min: 10,
-    max: 10,
-
-  },
-  email: {
-    type: String,
-    required: true,
-    lowecase: true,
-    match: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$,
-  },
-  city: {
-    type: String,
-    required: true,
-    minLength: 5,
-    maxLength: 25,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 4,
-  },
-},
-{
+  {
     timestamps: true,
-}
+  },
 );
 
 module.exports = mongoose.model('admin', adminSchema);
--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,10 +1,12 @@
-// const express = require('express');
-// const adminController= require ('../controllers/admins')
-// const validationsAdmins= require('../validations/admins')
+const express = require('express');
+const adminController = require('../controllers/admins');
+const validation = require('../validations/admins');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router
-// .get("/",adminController.getAllAdmins);
-// .get("/:id",adminController.getAllAdminsbyId);
-// .post("/",validation.validateAdminCreate,adminController.createAdmin)
+router
+  .get('/', adminController.getAllAdmins)
+  .get('/:id', adminController.getAdminById)
+  .post('/', validation.validateAdminCreate, adminController.createNewAdmin);
+
+module.exports = router;
