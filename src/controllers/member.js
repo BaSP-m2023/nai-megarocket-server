66c984f5af7b531d7209c251646cdd6990625653
eliezer-alberto <eliezer.alberto95@gmail.com>
2023-05-15 05:23:05 -0300

    MR-5: Validations added to controllers, but not finished.

--- src/controllers/member.js
+++ src/controllers/member.js
@@ -1,11 +1,67 @@
-// const updateMember = (req, res) => {
-//   console.log(req.params.id);
-// };
+const Member = require('../models/member');
 
-// const deleteMember = (req, res) => {
-//   console.log(req.params.id);
-// };
+const updateMember = (req, res) => {
+  const { id } = req.params;
+  const {
+    firsName,
+    lastName,
+    dni,
+    phone,
+    email,
+    password,
+    city,
+    birthDay,
+    postalCode,
+    isActive,
+    membership,
+  } = req.body;
 
-// module.exports = {
-//   updateMember, deleteMember,
-// };
+  Member.findByIdAndUpdate(
+    id,
+    {
+      firsName,
+      lastName,
+      dni,
+      phone,
+      email,
+      password,
+      city,
+      birthDay,
+      postalCode,
+      isActive,
+      membership,
+    },
+    { new: true },
+  )
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          msg: `The member with id: ${id} was not found`,
+        });
+      }
+      return res.status(200).json(result);
+    })
+    .catch((error) => res.status(400).json(error));
+};
+
+const deleteMember = (req, res) => {
+  const { id } = req.params;
+  Member.findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          msg: `The member with id: ${id} was not found`,
+        });
+      }
+      return res.status(204);
+    })
+    .catch((error) => res.status(400).json({
+      message: 'Something went wrong! ',
+      error,
+    }));
+};
+
+module.exports = {
+  updateMember,
+  deleteMember,
+};
