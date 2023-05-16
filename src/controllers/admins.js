d010d4961e6bd51d4b178c3cc568ac55081e0d06
francoax <francoa.duarte2001@gmail.com>
2023-05-16 18:05:12 -0300

    MR-02: endpoints remake and finished.

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -1,6 +1,99 @@
 const { default: mongoose } = require('mongoose');
 const Admin = require('../models/admins');
 
+const getAllAdmins = (req, res) => {
+  Admin.find()
+    .then((adminsList) => {
+      if (!adminsList) {
+        return res.status(404).json({
+          message: 'There are not admins yet',
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: 'Admins list obtained',
+        data: adminsList,
+        error: false,
+      });
+    })
+    .catch((err) => res.status(500).json({
+      message: 'An error has ocurred',
+      err,
+      error: true,
+    }));
+};
+
+const getAdminById = (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      message: 'The ID is not valid',
+      data: id,
+      error: true,
+    });
+  }
+  return Admin.findById(id)
+    .then((adminToSend) => {
+      if (!adminToSend) {
+        return res.status(404).json({
+          message: `Admin with id (${id}) was not found`,
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: 'Admin found',
+        data: adminToSend,
+        error: false,
+      });
+    })
+    .catch((err) => res.status(500).json({
+      message: 'An error has ocurred',
+      err,
+      error: true,
+    }));
+};
+
+const createAdmin = async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    dni,
+    phone,
+    email,
+    city,
+    password,
+  } = req.body;
+  const alreadyExists = await Admin.findOne({ $or: [{ dni }, { email }] });
+  if (alreadyExists) {
+    return res.status(400).json({
+      message: 'It already exists another admin with that Dni or Email',
+      data: req.body,
+      error: true,
+    });
+  }
+  return Admin.create({
+    firstName,
+    lastName,
+    dni,
+    phone,
+    email,
+    city,
+    password,
+  })
+    .then((adminCreated) => res.status(201).json({
+      message: 'Admin created',
+      data: adminCreated,
+      error: false,
+    }))
+    .catch((err) => res.status(500).json({
+      message: 'An error has ocurred',
+      err,
+      error: true,
+    }));
+};
+
 const updateAdmin = async (req, res) => {
   const { id } = req.params;
   if (!mongoose.isValidObjectId(id)) {
@@ -116,4 +209,7 @@ const deleteAdmin = (req, res) => {
 module.exports = {
   updateAdmin,
   deleteAdmin,
+  getAllAdmins,
+  getAdminById,
+  createAdmin,
 };
