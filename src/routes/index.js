db7401164c33deead58ffb06b4a19e3ac89aa6d1
francoax <francoa.duarte2001@gmail.com>
2023-05-16 17:02:48 -0300

    MR-03: routes index fixed.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+// const classes = require('./class');
 const subscriptions = require('./subscription');
 const admins = require('./admins');
 const members = require('./member');
 const superAdmins = require('./super-admins');
-// const classes = require('./class');
 const trainers = require('./trainer');
 const activities = require('./activity');
 
+// router.use('/classes', classes);
 router.use('/members', members);
 router.use('/admins', admins);
 router.use('/super-admins', superAdmins);
-// router.use('/classes', classes);
 router.use('/subscriptions', subscriptions);
 router.use('/trainers', trainers);
 router.use('/activities', activities);
