0d1efef78c52ed32e874c130a99d97763a738e93
Ignacio <igna.galceran@gmail.com>
2023-05-16 13:02:52 -0300

    MR-13: apply function to catch errors

--- src/controllers/super-admins.js
+++ src/controllers/super-admins.js
@@ -55,7 +55,7 @@ const updateSuperAdmin = (req, res) => {
             ));
         });
     })
-    .catch((error) => res.status(500).json({ msg: error.message, error: true }));
+    .catch((error) => applyResponse(res, 500, error.message, undefined, true));
 };
 
 const deleteSuperAdmin = (req, res) => {
@@ -70,7 +70,7 @@ const deleteSuperAdmin = (req, res) => {
       }
       return applyResponse(res, 200, `Super Admin ${result.firstName} was deleted`, result, false);
     })
-    .catch((error) => res.status(500).json({ msg: error.message, error: true }));
+    .catch((error) => applyResponse(res, 500, error.message, undefined, true));
 };
 
 module.exports = { updateSuperAdmin, deleteSuperAdmin };
