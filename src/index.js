695b6759dd951a5a4d8e2c7c8d3a22a5ccebf7b2
francoax <francoa.duarte2001@gmail.com>
2023-05-18 18:39:48 -0300

    MR-17: new option on mongoose connect.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import app from './app';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB_CONNECT_URL)
+mongoose.connect(process.env.MONGO_DB_CONNECT_URL, { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
 // eslint-disable-next-line no-console
   .then(() => console.log('MongoDB connected'))
 // eslint-disable-next-line no-console
-  .catch((e) => console.log(e));
+  .catch((err) => console.log('Error', err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
