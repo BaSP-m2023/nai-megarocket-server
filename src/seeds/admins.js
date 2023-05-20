4fcd1ed2a985f20bcf2986bcf710abbe9f5708ca
Ignacio <igna.galceran@gmail.com>
2023-05-20 12:43:13 -0300

    MR-30: update and delete tests completed

---
+++ src/seeds/admins.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('6465125fb6b5507c22ad8dcd'),
+    firstName: 'admin',
+    lastName: 'admin',
+    dni: 33333343,
+    phone: 1235467855,
+    email: 'testina@gmail.com',
+    city: 'Rosario',
+    password: 'Testerfacil123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6465125fb6b5507c22ad8dce'),
+    firstName: 'admin2',
+    lastName: 'admin2',
+    dni: 33323343,
+    phone: 1225467855,
+    email: 'testing@gmail.com',
+    city: 'Rosario',
+    password: 'Testerfacil123',
+  },
+];
--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -1,1 +1,90 @@
-test.skip('skip', () => {});
+import request from 'supertest';
+import app from '../app';
+import Admin from '../models/admins';
+import adminSeed from '../seeds/admins';
+
+const mockAdmin = {
+  firstName: 'admin',
+  lastName: 'admin',
+  dni: 22105409,
+  phone: 1235467855,
+  email: 'ignacio@gmail.com',
+  city: 'Rosario',
+  password: 'Testerfacil123',
+};
+
+const idInvalid = '6465113fb6';
+
+beforeAll(async () => {
+  await Admin.collection.insertMany(adminSeed);
+});
+
+describe('PUT /api/admins', () => {
+  test('should return status 400 dni already exists', async () => {
+    const response = await request(app).put('/api/admins/6465125fb6b5507c22ad8dcd').send({ dni: 33323343 });
+    expect(response.body.message).toBe('There is another admin with that data.');
+    expect(typeof response.body.data).toBe('object');
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 400 email already exists', async () => {
+    const response = await request(app).put('/api/admins/6465125fb6b5507c22ad8dcd').send({ firstName: 'admin2', email: 'testing@gmail.com' });
+    expect(response.body.message).toBe('There is another admin with that data.');
+    expect(typeof response.body.data).toBe('object');
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 400 due to invalid id', async () => {
+    const response = await request(app).put(`/api/admins/${idInvalid}`).send(mockAdmin);
+    expect(response.body.message).toBe('The ID is not valid');
+    expect(response.body.data).toBe(idInvalid);
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 404 admin not found', async () => {
+    const response = await request(app).put('/api/admins/6264125fb6b5507c22ad8dce').send(mockAdmin);
+    expect(response.body.message).toBe('Admin with the id (6264125fb6b5507c22ad8dce) was not found.');
+    expect(response.body.data).toBe(undefined);
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 400 due to empty body', async () => {
+    const response = await request(app).put('/api/admins/6465125fb6b5507c22ad8dce').send({});
+    expect(response.body.message).toBe('There is nothing to change');
+    expect(typeof response.body.data).toBe('object');
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 200 admin updated', async () => {
+    const response = await request(app).put('/api/admins/6465125fb6b5507c22ad8dce').send(mockAdmin);
+    expect(response.body.message).toBe('Admin updated');
+    expect(typeof response.body.data).toBe('object');
+    expect(response.status).toBe(200);
+    expect(response.error).toBeFalsy();
+  });
+});
+
+describe('PUT /api/admins', () => {
+  test('should return status 404 id not found', async () => {
+    const response = await request(app).delete('/api/admins/6465113fb6b5507c22ad8dcd').send();
+    expect(response.body.message).toBe('Admin with ID (6465113fb6b5507c22ad8dcd) was not found');
+    expect(response.body.data).toBe(undefined);
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 400 id is not valid', async () => {
+    const response = await request(app).delete(`/api/admins/${idInvalid}`).send();
+    expect(response.body.message).toBe('The ID is not valid');
+    expect(response.body.data).toBe(idInvalid);
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('should return status 200 admin deleted', async () => {
+    const response = await request(app).delete('/api/admins/6465125fb6b5507c22ad8dcd').send();
+    expect(response.body.message).toBe('Admin deleted');
+    expect(typeof response.body.data).toBe('object');
+    expect(response.body.message).toBe('Admin deleted');
+    expect(response.status).toBe(200);
+    expect(response.error).toBeFalsy();
+  });
+});
