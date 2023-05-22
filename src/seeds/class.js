6fbfb0d7c8ac32ee6ddad5760a3bec316b9be6ba
Gianluca Agrano <gianlucka1@gmail.com>
2023-05-21 22:58:28 -0300

    MR-32: Added class seed

---
+++ src/seeds/class.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('646a353ecdc4cb3be6be10b5'),
+    day: 'Monday',
+    hour: '17:00',
+    trainer: '646428fc0b6aa64a90624a21',
+    activity: '6465095739daaa5e7a21031a',
+    slots: 7,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('646a353ecdc4cb3be6be10a7'),
+    day: 'Monday',
+    hour: '17:00',
+    trainer: '646428fc0b6aa64a90624c05',
+    activity: '6465095739daaa5e7a21051a',
+    slots: 7,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('646a353ecdc4cb3be6be10c3'),
+    day: 'Monday',
+    hour: '17:00',
+    trainer: '646428fc0b6aa64a90624b07',
+    activity: '6465095739daaa5e7a21041a',
+    slots: 7,
+  },
+];
