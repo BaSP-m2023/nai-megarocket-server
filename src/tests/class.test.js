6d010d69df13712757ddbdf0fe8a939817e677c1
unknown <ivanjukonis@hotmail.com>
2023-05-22 18:07:59 -0300

    MR-31: Test added into GET endpoint.

--- src/tests/class.test.js
+++ src/tests/class.test.js
@@ -8,8 +8,41 @@ beforeAll(async () => {
 });
 
 describe('GET /api/classes', () => {
-  test('should return status 200', async () => {
+  test('Should return status 200 and false error.', async () => {
     const response = await request(app).get('/api/classes').send();
     expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+  });
+  test('Should return an object containing an array of classes.', async () => {
+    const response = await request(app).get('/api/classes').send();
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.data).toEqual(expect.arrayContaining([]));
+    expect(response.body.error).toBeFalsy();
+  });
+  test('List of classes should not be empty.', async () => {
+    const response = await request(app).get('/api/classes').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.error).toBeFalsy();
+  });
+  test('Should return a message indicating "Complete list of classes".', async () => {
+    const response = await request(app).get('/api/classes').send();
+    expect(response.body.message).toBe('Complete list of classes.');
+  });
+  test('Should return status 404 and truth error.', async () => {
+    const response = await request(app).get('/api/classe').send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+  test('Should return all properties of classes.', async () => {
+    const response = await request(app).get('/api/classes').send();
+    const classes = response.body.data;
+    classes.forEach((classItem) => {
+      expect(classItem).toHaveProperty('day');
+      expect(classItem).toHaveProperty('hour');
+      expect(classItem).toHaveProperty('trainer');
+      expect(classItem).toHaveProperty('activity');
+      expect(classItem).toHaveProperty('slots');
+    });
+    expect(response.error).toBeFalsy();
   });
 });
