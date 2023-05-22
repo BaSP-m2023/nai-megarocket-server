506e102de7508a5cec792c3d24087229842ef25e
Federico Cavallo <fcmde1995@gmail.com>
2023-05-22 00:04:51 -0300

    MR-29:More test added

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -36,26 +36,28 @@ beforeAll(async () => {
 });
 
 describe('GET /api/admins', () => {
-  test('should return status 200', async () => {
+  test('should return status 200 and length to be 2', async () => {
     const response = await request(app).get('/api/admins').send();
-    expect(response.status).toBe(200);
-  });
-  test('Admins length should be 2', async () => {
-    const response = await request(app).get('/api/admins').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
     expect(response.body.data.length).toBe(2);
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body).toBeDefined();
+    expect(response.body.message).toBeDefined();
   });
   test('should return status 404, due to invalid url', async () => {
     const response = await request(app).get('/api/admin').send();
     expect(response.status).toBe(404);
+    expect(response.body).toBeDefined();
   });
   test('should return status 404, as there are no admins in the DB', async () => {
     await Admin.collection.deleteMany();
     const response = await request(app).get('/api/admins').send();
     expect(response.status).toBe(404);
-  });
-  test('Admins length should be 0', async () => {
-    const response = await request(app).get('/api/admins').send();
+    expect(response.body.error).toBe(true);
+    expect(response.body).toBeDefined();
     expect(response.body.data.length).toBe(0);
+    expect(response.body.message).toBeDefined();
   });
 });
 
@@ -64,14 +66,22 @@ describe('GETBYID /api/admins/', () => {
     await Admin.collection.insertMany(adminSeed);
     const response = await request(app).get('/api/admins/646abc7bb96405301276e34f').send();
     expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body).toBeDefined();
+    expect(response.body.message).toBeDefined();
   });
   test('should return status 404 as the id is not in the DB', async () => {
     const response = await request(app).get('/api/admins/6468483feee5aba1cd3f748b').send();
     expect(response.status).toBe(404);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.data).toBeUndefined();
   });
   test('should return status 400 as the id is Invalid', async () => {
     const response = await request(app).get('/api/admins/hola soy un id').send();
     expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBeDefined();
   });
   test('should return status 404, due to invalid url', async () => {
     const response = await request(app).get('/api/admin/').send();
@@ -83,18 +93,27 @@ describe('POST /api/admins', () => {
   test('should create an admin return status 201', async () => {
     const response = await request(app).post('/api/admins').send(mockAdmin);
     expect(response.status).toBe(201);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body).toBeDefined();
+    expect(response.body.message).toBeDefined();
   });
   test('should create an admin return status 400 due to same email', async () => {
     const response = await request(app).post('/api/admins').send(mockAdminEmail);
     expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBeDefined();
   });
   test('should create an admin return status 400 due to same dni', async () => {
     const response = await request(app).post('/api/admins').send(mockAdminDni);
     expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBeDefined();
   });
   test('should return 400 as no body is send', async () => {
     const response = await request(app).post('/api/admins').send();
     expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBeDefined();
   });
   test('should return status 404, due to invalid url', async () => {
     const response = await request(app).post('/api/admin').send();
