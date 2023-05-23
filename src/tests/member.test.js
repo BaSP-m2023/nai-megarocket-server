406becda9955b77c99b2cf3271b3bdb48893e2d7
Daniel Lezama <dlezama0796@gmail.com>
2023-05-22 22:42:47 -0300

    MR-33: fixes in response.body.error

--- src/tests/member.test.js
+++ src/tests/member.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import app from '../app';
 import Member from '../models/member';
@@ -17,7 +18,7 @@ const mockMember = {
   membership: 'Black',
 };
 
-/* const mockRepeatedEmail = {
+const mockRepeatedEmail = {
   firstName: 'Adoslf',
   lastName: 'Rodsriguez',
   dni: 34053981,
@@ -29,7 +30,7 @@ const mockMember = {
   postalCode: 85049,
   isActive: true,
   membership: 'Black',
-}; */
+};
 
 beforeEach(async () => {
   await Member.collection.insertMany(memberSeed);
@@ -44,12 +45,7 @@ describe('GET /api/members', () => {
     const response = await request(app).get('/api/members').send();
     expect(response).toBeTruthy();
     expect(response.status).toBe(200);
-    expect(response.error).toBeFalsy();
-  });
-  test('If wrong URL status 404', async () => {
-    const response = await request(app).get('/api/member').send();
-    expect(response.status).toBe(404);
-    expect(response.error).toBeTruthy();
+    expect(response.body.error).toBeFalsy();
   });
   test('If no members, correct error message', async () => {
     Member.collection.deleteMany();
@@ -58,18 +54,22 @@ describe('GET /api/members', () => {
     expect(response.status).toBe(404);
     expect(response.body.message).toBe('No Members on the list, please create one.');
   });
+  test('If wrong URL status 404', async () => {
+    const response = await request(app).get('/api/member').send();
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBeTruthy();
+  });
 });
 
 describe('GET BY ID /api/members/:id', () => {
   test('If correct ID should found member', async () => {
     const firstMember = await Member.findOne();
-    // eslint-disable-next-line no-underscore-dangle
     const firstId = firstMember._id;
     const response = await request(app).get(`/api/members/${firstId}`).send();
     expect(response).toBeTruthy();
     expect(response.status).toBe(200);
     expect(response.body.data).toHaveProperty('_id', 'firstName', 'lastName', 'dni', 'phone', 'email', 'password', 'city', 'birthDay', 'postalCode', 'isActive', 'membership');
-    expect(response.error).toBeFalsy();
+    expect(response.body.error).toBeFalsy();
   });
   test('If incorrect ID, gives correct error message', async () => {
     const incorrectId = '645ea2e59338b2a843989320';
@@ -81,7 +81,7 @@ describe('GET BY ID /api/members/:id', () => {
   test('If wrong URL status 404', async () => {
     const response = await request(app).get('/api/member/:id').send();
     expect(response.status).toBe(404);
-    expect(response.error).toBeTruthy();
+    expect(response.body.error).toBeTruthy();
   });
 });
 
@@ -90,11 +90,16 @@ describe('POST /api/members', () => {
     const response = await request(app).post('/api/members').send(mockMember);
     expect(response.status).toBe(201);
     expect(response.body.data).toHaveProperty('_id', 'firstName', 'lastName', 'dni', 'phone', 'email', 'password', 'city', 'birthDay', 'postalCode', 'isActive', 'membership');
-    expect(response.error).toBeFalsy();
+    expect(response.body.error).toBeFalsy();
   });
-  /* test('Error 400 if repeated email', async () => {
+  test('Error 400 if repeated email', async () => {
     const response = await request(app).post('/api/members').send(mockRepeatedEmail);
     expect(response.status).toBe(400);
     expect(response.body.error).toBe('Email already exists in the database, please check.');
-  }); */
+  });
+  test('If wrong URL status 404', async () => {
+    const response = await request(app).post('/api/member').send();
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBeTruthy();
+  });
 });
