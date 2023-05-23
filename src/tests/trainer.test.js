9141eb8a0b46f5dbbf897742953e90f10a1416e9
francoax <francoa.duarte2001@gmail.com>
2023-05-23 12:43:06 -0300

    MR-40: fix on trainers.test

--- src/tests/trainer.test.js
+++ src/tests/trainer.test.js
@@ -58,34 +58,6 @@ afterEach(async () => {
   await Trainer.collection.deleteMany();
 });
 
-describe('DELETE /api/trainers/:id', () => {
-  test('should return status 400 if the ID is invalid', async () => {
-    const mockId = '@a@invalid@id';
-    const response = await request(app).delete(`/api/trainers/${mockId}`).send();
-    expect(response.status).toBe(400);
-    expect(response.body.message).toBeDefined();
-    expect(response.body.message).toMatch(/(invalid)/gi);
-    expect(response.body.error).toBeTruthy();
-  });
-
-  test('should return status 404 if is not found', async () => {
-    const mockId = '646428fc0b6aa64a90624c00';
-    const response = await request(app).delete(`/api/trainers/${mockId}`).send();
-    expect(response.status).toBe(404);
-    expect(response.body.message).toBeDefined();
-    expect(response.body.error).toBeTruthy();
-  });
-
-  test('should return status 200 if the trainer was deleted', async () => {
-    const { _id } = await Trainer.collection.findOne();
-    const response = await request(app).delete(`/api/trainers/${_id}`).send();
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBeDefined();
-    expect(response.body.message).toMatch(/(deleted)/gi);
-    expect(response.body.error).toBeFalsy();
-  });
-});
-
 describe('GET /api/trainers', () => {
   test('Should bring data', async () => {
     const response = await request(app).get('/api/trainers').send();
@@ -219,6 +191,34 @@ describe('PUT /api/trainers/:id', () => {
     expect(response.status).toBe(200);
     expect(response.body.message).toBeDefined();
     expect(response.body.message).toMatch(/(updated)/gi);
+    expect(response.body.error).toBeFalsy();
+  });
+});
+
+describe('DELETE /api/trainers/:id', () => {
+  test('should return status 400 if the ID is invalid', async () => {
+    const mockId = '@a@invalid@id';
+    const response = await request(app).delete(`/api/trainers/${mockId}`).send();
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.message).toMatch(/(invalid)/gi);
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('should return status 404 if is not found', async () => {
+    const mockId = '646428fc0b6aa64a90624c00';
+    const response = await request(app).delete(`/api/trainers/${mockId}`).send();
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('should return status 200 if the trainer was deleted', async () => {
+    const { _id } = await Trainer.collection.findOne();
+    const response = await request(app).delete(`/api/trainers/${_id}`).send();
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBeDefined();
+    expect(response.body.message).toMatch(/(deleted)/gi);
     expect(response.body.error).toBeFalsy();
   });
 });
